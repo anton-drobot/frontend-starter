6d31cd3b008bb5e11907ab73a26abafd1d9ed14c
Anton Drobot <anton-drobot@users.noreply.github.com>
2017-06-06 01:54:40 +0500

    fix(lang): possible to insert markup into translation strings

--- framework/Lang/index.js
+++ framework/Lang/index.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import get from 'lodash/get';
 
 class Lang {
@@ -55,7 +56,40 @@ class Lang {
     get(key, params = {}) {
         const compiledKey = get(this.translations(), key);
 
-        return compiledKey ? compiledKey(params) : `lang::${key}`;
+        if (!compiledKey) {
+            return `lang::${key}`;
+        }
+
+        // Need to insert React Markup into string
+        const transpiledParams = {};
+        let hasReactParams = false;
+
+        Object.keys(params).forEach((paramName) => {
+            if (React.isValidElement(params[paramName])) {
+                transpiledParams[paramName] = `$ReactComponent:${paramName}$`;
+                hasReactParams = true;
+            } else {
+                transpiledParams[paramName] = params[paramName];
+            }
+        });
+
+        const string = compiledKey(transpiledParams);
+
+        if (!hasReactParams) {
+            return string;
+        }
+
+        return string
+            .split(/\$(ReactComponent:.*?)\$/g)
+            .map((part) => {
+                if (part.indexOf('ReactComponent:') !== 0) {
+                    return part;
+                }
+
+                const paramName = part.substr('ReactComponent:'.length);
+
+                return params[paramName];
+            });
     }
 
     /**
