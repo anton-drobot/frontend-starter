5891f01eb297f4b4a6adadf1d7d4f2d409b945ae
Anton Drobot <anton-drobot@users.noreply.github.com>
2017-06-14 16:47:11 +0500

    feat(server): process clustering

--- server.js
+++ server.js
@@ -1,3 +1,27 @@
+import cluster from 'cluster';
+import os from 'os';
+import CatLog from 'cat-log';
 import registerServer from 'bootstrap/server';
 
-registerServer();
+const log = new CatLog('server');
+const numCPUs = os.cpus().length;
+
+if (process.env.NODE_ENV === 'production') {
+    if (cluster.isMaster) {
+        log.info(`Master ${process.pid} is running`);
+
+        // Fork workers.
+        for (let i = 0; i < numCPUs; i++) {
+            cluster.fork();
+        }
+
+        cluster.on('exit', (worker, code, signal) => {
+            log.info(`Worker ${worker.process.pid} died`);
+        });
+    } else {
+        registerServer();
+        log.info(`Worker ${process.pid} started`);
+    }
+} else {
+    registerServer();
+}
