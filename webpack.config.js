c2ac3c287d61f3cca719fbc5745067e6fc91fe30
Anton Drobot <anton-drobot@users.noreply.github.com>
2017-11-19 23:55:41 +0500

    feat(flow): add flow

--- webpack.config.js
+++ webpack.config.js
@@ -97,6 +97,7 @@ module.exports = {
                                             ]
                                         }
                                     ],
+                                    'flow',
                                     'react'
                                 ],
                                 plugins: [
