d6b9f3508fe4147e6c037784783a6f4d1ac41be3
Anton Drobot <anton-drobot@users.noreply.github.com>
2018-01-04 18:58:39 +0300

    test(browserHistory): add tests

---
+++ framework/utils/__tests__/browserHistory.test.js
@@ -0,0 +1,41 @@
+import noop from 'lodash/noop';
+import { createHistory } from '../browserHistory';
+
+jest.mock('../../IoC/inject');
+
+describe('browserHistory', () => {
+    describe('createHistory', () => {
+        test('server side', () => {
+            const env = {
+                isClientSide: false
+            };
+
+            expect(createHistory(env)).toEqual({
+                push: noop,
+                replace: noop
+            });
+        });
+
+        test('client side', () => {
+            window.history.pushState = jest.fn();
+            window.history.replaceState = jest.fn();
+
+            const env = {
+                isClientSide: true
+            };
+
+            const history = createHistory(env);
+
+            expect(history).not.toEqual({
+                push: noop,
+                replace: noop
+            });
+
+            history.push('http://localhost:3000/test');
+            expect(window.history.pushState).toHaveBeenCalledWith({ url: 'http://localhost:3000/test' }, 'http://localhost:3000/test', 'http://localhost:3000/test');
+
+            history.replace('http://localhost:3000/test');
+            expect(window.history.replaceState).toHaveBeenCalledWith({ url: 'http://localhost:3000/test' }, 'http://localhost:3000/test', 'http://localhost:3000/test');
+        });
+    });
+});
--- framework/utils/browserHistory.js
+++ framework/utils/browserHistory.js
@@ -5,26 +5,29 @@ import noop from 'lodash/noop';
 import inject from '../IoC/inject';
 import { ENV_PROVIDER } from '../Providers/types';
 
+function push(url) {
+    window.history.pushState({ url }, url, url);
+}
+
+function replace(url) {
+    window.history.replaceState({ url }, url, url);
+}
+
 /**
  * Create HistoryAPI for browser or emulation for server.
  *
- * @return {{push: Function|noop, replace: Function|noop}}
+ * @return {{push: push|noop, replace: replace|noop}}
  */
 export const createHistory = inject(ENV_PROVIDER)(function (env: Object): { push: (url: string) => void, replace: (url: string) => void } {
-    const history = {
-        push: noop,
-        replace: noop
-    };
-
     if (env.isClientSide) {
-        history.push = (url) => {
-            window.history.pushState({ url }, url, url);
-        };
-
-        history.replace = (url) => {
-            window.history.replaceState({ url }, url, url);
+        return {
+            push,
+            replace
         };
     }
 
-    return history;
+    return {
+        push: noop,
+        replace: noop
+    };
 });
